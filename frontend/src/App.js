40c70e0a248d83ddcc008aa368780d918030d993
Manjunath M <manjunath.m@ansrsource.com>
2021-04-07 16:29:30 +0530

    front end Complteted for Components (Header, Foooter, Product, Rating) and screens (Homescreen and Productscreen)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {Container} from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
  const App = () => {
   return (
-    <>
-      <h1>Welcome to eshop</h1>
-    </>
+    <Router>
+    <Header/>
+    <main className="py-3">
+      <Container>
+        <Route path="/" exact component={HomeScreen}/>
+      <Route path="/product/:id" component={ProductScreen}/>
+      </Container>
+    </main>
+    
+      <Footer/>
+      </Router>
   )
 }
 
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from "prop-types"
+const Rating = ({value, totalValue, color}) => {
+    const ratingsList=[1,2,3,4,5]
+    return (
+        <div className="rating">
+            {ratingsList.map((rating=>(<span key={rating}>
+                <i style={{color:color}} className={value >=rating? 'fas fa-star' : value>=(rating-0.5)? 'fas fa-star-half-alt' : 'far fa-star' }/>
+            </span>)))}
+            {/* <span>
+                <i className={value >=1? "fa fa-star" : value>=0.5? "fa fa-star-half-o" : "fa fa-star-o" }/>
+            </span>
+            <span>
+                <i className={value>=2?"fa fa-star" : value>=1.5? "fa fa-star-half-o" : "fa fa-star-o" }/>
+            </span>
+            <span>
+                <i className={value>=3?"fa fa-star" : value>=2.5? "fa fa-star-half-o" : "fa fa-star-o" }/>
+            </span>
+            <span>
+                <i className={value>=4?"fa fa-star" : value>=3.5? "fa fa-star-half-o" : "fa fa-star-o" }/>
+            </span>
+            <span>
+                <i className={value>=5?"fa fa-star" : value>=4.5? "fa fa-star-half-o" : "fa fa-star-o" }/>
+            </span> */}
+        <span>{totalValue && `${totalValue} reviews`}</span>
+        </div>
+    )
+}
+
+Rating.defaultProps={
+    color:"#f8e825"
+}
+Rating.propTypes={
+    value: PropTypes.number.isRequired,
+    totalValue: PropTypes.number.isRequired,
+    color:PropTypes.string
+}
+
+export default Rating
