7e9504c5ab5276c6fde324865b713f33cfa7a753
Manjunath M <manjunath.m@ansrsource.com>
2021-04-23 18:12:56 +0530

    Custom Error handling

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from"express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import productRouter from './routes/productRoute.js'
+import {notFound,errorhandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
 const app= express()
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,7 +16,12 @@ app.get('/', (req, res) => {
 
 app.use("/api/products",productRouter)
 
-  const PORT = process.env.PORT || 5000
+
+//middleware is a function that have access to req res cycles
+app.use(notFound)
+app.use(errorhandler)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
