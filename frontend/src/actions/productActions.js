db9b4a7b87491778bf3fcb5149ce289e640c96c1
Manjunath M <manjunath.m@ansrsource.com>
2021-04-27 15:18:12 +0530

    Created Cart Reducer and Cart Action and storing the cartitems in localstorage

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import {PRODUCT_LIST_FAIL,PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL} from '../constants/productConstants'
 
+//using thunk calling arrow function inside a funtion. 
+//Redux Thunk middleware allows you to write action creators that return a function instead of an action.
+//The inner function receives the store methods dispatch(used for async )and getstate(used for conditional).
 export const listProducts = ()=> async (dispatch)=>{
         try {
             dispatch({type: PRODUCT_LIST_REQUEST})
